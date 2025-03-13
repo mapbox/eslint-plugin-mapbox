4375fc202a00fd3eee6e93201d51ea0b0ee974d7
Oana Badiu <oana.badiu@mapbox.com>
2025-03-13 17:08:50 +0200

    Feedback

--- rules/detect-token.js
+++ rules/detect-token.js
@@ -15,32 +15,28 @@ module.exports = {
   },
   create(context) {
     const tokenPattern =
-      /\b(?:tk|pk|sk)\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]{10,}\b/g;
+      /\b(tk|pk|sk)\.([A-Za-z0-9-_]+)\.([A-Za-z0-9-_]{10,})\b/g;
 
     const checkString = (node, value) => {
-      const matches = value.match(tokenPattern);
+      const matches = [...value.matchAll(tokenPattern)];
 
-      if (matches) {
-        matches.forEach((token) => {
-          const parts = token.split('.');
+      matches.forEach(([token, tp, payload]) => {
+        context.report({
+          node,
+          messageId: 'forbiddenToken',
+          data: { token },
+          fix(fixer) {
+            const fixedToken = `${tp}.${payload}.test`;
 
-          context.report({
-            node,
-            messageId: 'forbiddenToken',
-            data: { token },
-            fix(fixer) {
-              const fixedToken = `${parts[0]}.${parts[1]}.test`;
-
-              if (node.type === 'JSXText') {
-                return fixer.replaceText(node, fixedToken);
-              }
-
-              const fixedValue = value.replace(token, fixedToken);
-              return fixer.replaceText(node, JSON.stringify(fixedValue));
+            if (node.type === 'JSXText') {
+              return fixer.replaceText(node, fixedToken);
             }
-          });
+
+            const fixedValue = value.replace(token, fixedToken);
+            return fixer.replaceText(node, JSON.stringify(fixedValue));
+          }
         });
-      }
+      });
     };
 
     return {
